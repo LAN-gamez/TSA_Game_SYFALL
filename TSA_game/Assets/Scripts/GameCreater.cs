9ce3fb68ee9faab998f9ed2911cf8c289da89508
LAN-gamez <92351748+LAN-gamez@users.noreply.github.com>
2023-03-06 18:02:34 -0700

    Thanks Tyler

--- TSA_game/Assets/Scripts/GameCreater.cs
+++ TSA_game/Assets/Scripts/GameCreater.cs
@@ -1,39 +1,106 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using TMPro;
+using System.Linq;
+
 
 public class GameCreater : MonoBehaviour 
 {
-    private int playerCount;
+
+    public TextMeshProUGUI locationLabel;
+    public Image locationImage;
+    public TextMeshProUGUI characterLabel;
+
+    public List<Sprite> locationImages;
+
+    private string[] possibleLocations = new string[] { "barracks", "boat", "bountyBoard", "church", "docks", "forges", "gallows", "graveyard", "inn", "market", "mines", "palaceDungeon", "tavern", "throneRoom", "witchHut" };
+
+
+    private int playerCount = 3;
     private string[] playerCharacters;
+
+    private string currentLocation;
+    private int spyIndex;
+
+    private int currentShowingRole = -1;
+
+     
     public void HandleInputData(int val)
     {
-        playerCount = val + 2;
+        playerCount = val + 3;
+        print("handle input data " + val);
     }
-    public void locations()
-    {
-        string[] tavern = {"Musician", "Bartender", "Drunk", "Tavern Keeper", "Security Guard", "Dancer" };
-        string[] throneRoom = { "King/Queen", "Courier", "Jester", "Royale Bodyguard", "Royale Advisor", "Royale Servant" };
-        string[] market = { "Shopkeeper", "Tax Collector", "Coachman", "Traveling Merchant", "Peasant", "Smith" };
-        string[] inn = { "Innkeeper", "Maid/manservant", "Traveler", "Bard", "Brewer", "Cook"};
-        string[] mines = { "Pick Sharpener", "Miner", "Lift Operator", "Water Boy", "Crystal Inspector", "Canary Holder" };
-        string[] forges = { "Blacksmith", "Apprentice", "Coal Runner", "Customer", "Swordsman", "Bellows Worker" };
-        string[] bountyBoard = { "Guild representative", "Famous Adventurer", "Adventurer wanna-be", "Wizzard", "Half-ling", "Archer" };
-        string[] docks = { "Ship Captain", "Barnacle Boy", "Merchant", "Fisher", "Net Mender", "Docking Master" };
-        string[] church = { "Innkeeper", "Worshipper", "Orphan", "Deacon", "Singer", "Paladin" };
-        string[] graveyard = { "Mortician", "Vampire", "Gravedigger", "Necromancer", "Corpse", "Ghost" };
-        string[] gallows = { "Executioner", "Priest", "Guards", "Citizen", "Condemned", "Physician" };
-        string[] barracks = { "Quartermaster", "Cook", "Recruit", "Platoon Leader", "Drunk", "Trainer" };
-        string[] boat = { "Captain", "First Mate", "Cook", "Quartermaster", "Cartographer", "Carpenter" };
-        string[] witchHut = { "Witch", "Broom", "Cat", "Raven", "Shrunken Head", "Customer" };
-        string[] palaceDungeon = { "Guard", "Drunk", "Madman", "Witch", "Thief", "Prisoner" };
 
-        object[] location = {tavern, throneRoom, market, inn, mines, forges, bountyBoard,
-            docks, church, graveyard, gallows, barracks, boat, witchHut, palaceDungeon};
-    }
-    public void chooseLocation()
-    {
         
+    public void GameSetup()
+    {
+        Dictionary<string, List<string>> locations = new Dictionary<string, List<string>>()
+        {
+            { "barracks", new List<string>() { "Quartermaster", "Cook", "Recruit", "Platoon Leader", "Drunk", "Trainer" } },
+            { "boat", new List<string>() { "Captain", "First Mate", "Cook", "Quartermaster", "Cartographer", "Carpenter" } },
+            { "bountyBoard", new List<string>() {  "Guild representative", "Famous Adventurer", "Adventurer wanna-be", "Wizzard", "Half-ling", "Archer" } },
+            { "church", new List<string>() { "Priest", "Worshipper", "Orphan", "Deacon", "Singer", "Paladin" } },
+            { "docks", new List<string>() { "Ship Captain", "Barnacle Boy", "Merchant", "Fisher", "Net Mender", "Docking Master" } },
+            { "forges", new List<string>() { "Blacksmith", "Apprentice", "Coal Runner", "Customer", "Swordsman", "Bellows Worker" } },
+            { "gallows", new List<string>() { "Executioner", "Priest", "Guards", "Citizen", "Condemned", "Physician" } },
+            { "graveyard", new List<string>() { "Mortician", "Vampire", "Gravedigger", "Necromancer", "Corpse", "Ghost" } },
+            { "inn", new List<string>() { "Innkeeper", "Maid/manservant", "Traveler", "Bard", "Brewer", "Cook" } },
+            { "market", new List<string>() { "Shopkeeper", "Tax Collector", "Coachman", "Traveling Merchant", "Peasant", "Smith" } },
+            { "mines", new List<string>() {  "Pick Sharpener", "Miner", "Lift Operator", "Water Boy", "Crystal Inspector", "Canary Holder" } },
+            { "palaceDungeon", new List<string>() { "Guard", "Drunk", "Madman", "Witch", "Thief", "Prisoner" } },
+            { "tavern", new List<string>() {"Musician", "Bartender", "Drunk", "Tavern Keeper", "Security Guard", "Dancer" } },
+            { "throneRoom", new List<string>() { "King/Queen", "Courier", "Jester", "Royale Bodyguard", "Royale Advisor", "Royale Servant" } },
+            { "witchHut", new List<string>() {  "Witch", "Broom", "Cat", "Raven", "Shrunken Head", "Customer" } },
+        };
+
+        currentLocation = locations.Keys.ElementAt(Random.Range(0, locations.Count));
+        spyIndex = Random.Range(0, playerCount);
+        playerCharacters = new string[playerCount];
+
+        for (int i = 0; i < playerCount; i++)
+        {
+            if (i == spyIndex)
+            {
+                string assignment = locations[currentLocation][Random.Range(0, locations[currentLocation].Count)];
+                playerCharacters[i] = assignment;
+                locations[currentLocation].Remove(assignment);
+            }
+            else
+            {
+                playerCharacters[i] = "Weasel";
+            }
+        }
+
+        AssignRoles();
+    }
+
+    public void AssignRoles()
+    {
+        currentShowingRole++;
+        print(playerCharacters);
+        print("weasle index " + spyIndex);
+        print("current " + currentShowingRole);
+
+        if (currentShowingRole != spyIndex)
+        {
+            locationImage.enabled = true;
+            locationImage.sprite = locationImages[System.Array.IndexOf(possibleLocations, currentLocation)];
+            locationLabel.text = currentLocation;
+        }
+        else
+        {
+            locationImage.enabled = false;
+            locationLabel.text = "";
+        }
+
+        characterLabel.text = playerCharacters[currentShowingRole];
+        print("role " + playerCharacters[currentShowingRole]);
+
+        if (currentShowingRole == playerCount - 1)
+        {
+            print("FINISH");
+        }
     }
 }
